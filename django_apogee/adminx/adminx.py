61b64190fbe47c5321767a6fdd439db314f01641
paul <paul.guichon@iedparis8.net>
2014-10-17 17:25:07 +0200

    ajout limit auditeur, correction bug opi

--- django_apogee/adminx/adminx.py
+++ django_apogee/adminx/adminx.py
@@ -8,5 +8,7 @@ from django_apogee.models.models_configuration import ConfAnneeUni
 class IndividuAdmin(object):
     search_fields = ['lib_nom_pat_ind', 'lib_nom_usu_ind', 'lib_pr1_ind', 'cod_etu']
     list_display = ['lib_nom_pat_ind', 'lib_nom_usu_ind', 'lib_pr1_ind', 'cod_etu']
+
+
 xadmin.site.register(Individu, IndividuAdmin)
 xadmin.site.register(ConfAnneeUni)
