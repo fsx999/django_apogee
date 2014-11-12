b4b2f81ac2505c714e85c87375166c8fc5b378ba
paul <paul.guichon@iedparis8.net>
2014-11-12 14:26:16 +0100

    initial

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='django_apogee', version='0.0.1',
       packages=['', 'django_apogee', 'django_apogee.models', 'django_apogee.management',
-                'django_apogee.management.commands', 'django_apogee.migrations'],
+                'django_apogee.management.commands', 'django_apogee.south_migrations'],
       url='https://github.com/fsx999/django_apogee', license='bsd 3',
       author='paul guichon', author_email='paul.guichon@iedparis8.net', description='',
       install_requires=['django',
