a7c8b2976cea112978c5a53d760fa0367a007aee
paul guichon <paul.guichon@iedparis8.net>
2015-03-12 15:46:06 +0100

    ctu

--- django_apogee/management/commands/initialisation_base.py
+++ django_apogee/management/commands/initialisation_base.py
@@ -91,7 +91,7 @@ class Command(BaseCommand):
                 for x in p.page(page).object_list:
                     x.copy()
             print u"La table {} est copiee".format(model._meta.db_table)
-        query = InsAdmEtpInitial.objects.using(APOGEE_CONNECTION).filter(cod_cge=COD_CGE, COD_ANU__gt=2012) # si filter annee ajoute cod_anu__in=[mes annee]
+        query = InsAdmEtpInitial.objects.using(APOGEE_CONNECTION).filter(cod_cge=COD_CGE, cod_anu__gt=2012) # si filter annee ajoute cod_anu__in=[mes annee]
         p = Paginator(query, NB_COPY_INITIALISATION)
         for page in p.page_range:
             for x in p.page(page).object_list:
