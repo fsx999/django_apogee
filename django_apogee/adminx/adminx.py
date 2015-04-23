1446e367ce671a5d7b6509b3471b06311e5775fa
paul guichon <paul.guichon@iedparis8.net>
2015-04-23 16:27:59 +0200

    mise à jour des content type

--- django_apogee/adminx/adminx.py
+++ django_apogee/adminx/adminx.py
@@ -26,7 +26,7 @@ class InsAdmEtpInline(object):
         return False
 
 
-class IndividuAdmin(object):
+class InsAdmEtp(object):
     site_title = "Consultation des dossiers étudiants en Apogée"
     search_fields = ['lib_nom_pat_ind', 'lib_nom_usu_ind', 'lib_pr1_ind', 'cod_etu']
     list_display = ['identite', 'ine', 'cod_etu', 'get_etiquette']
@@ -36,7 +36,7 @@ class IndividuAdmin(object):
                        'lib_pr1_ind', 'lib_pr2_ind', 'lib_pr3_ind',
                        'cod_etu', 'get_code_secret', 'get_etiquette']
     style = 'table'
-    # inlines = [InsAdmEtpInline]
+    inlines = [InsAdmEtpInline]
     hidden_menu = True
 
     def has_add_permission(self):
